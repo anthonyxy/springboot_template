fb862f97b4f255ae03718c939b8072440296a858
jax <jax233@126.com>
2022-05-09 09:51:40 +0800

    架构修改

--- src/main/java/com/xyz/interceptor/Interceptor.java
+++ src/main/java/com/xyz/interceptor/Interceptor.java
@@ -2,21 +2,16 @@ package com.xyz.interceptor;
 
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.StrUtil;
-import com.xyz.config.SystemConfig;
-import com.xyz.util.ConstantUtil;
-import com.xyz.util.ResponseUtil;
-import com.xyz.util.dto.DataResult;
+import com.aliyuncs.utils.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-import sun.security.util.SignatureUtil;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
@@ -28,7 +23,7 @@ public class Interceptor implements HandlerInterceptor {
 
     // 预处理回调
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        String ip = ConstantUtil.getIp(request);
+        String ip = getIpAddress(request);
         String url = request.getRequestURI();
         // in日志
         comeIn(ip, url);
@@ -69,6 +64,41 @@ public class Interceptor implements HandlerInterceptor {
     // come out
     private void comeOut() {
         logger.info(new DateTime().toMsStr() + "-----Come Out-----");
+    }
+
+    // 获取请求的ip
+    private static String getIpAddress(HttpServletRequest request) {
+        String Xip = request.getHeader("X-Real-IP");
+        String XFor = request.getHeader("X-Forwarded-For");
+        if (StrUtil.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)) {
+            // 多次反向代理后会有多个ip值，第一个ip才是真实ip
+            int index = XFor.indexOf(",");
+            if (index != -1) {
+                return XFor.substring(0, index);
+            } else {
+                return XFor;
+            }
+        }
+        XFor = Xip;
+        if (StrUtil.isNotEmpty(XFor) && !"unKnown".equalsIgnoreCase(XFor)) {
+            return XFor;
+        }
+        if (StrUtil.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("Proxy-Client-IP");
+        }
+        if (StrUtil.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (StrUtil.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (StrUtil.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (StrUtil.isBlank(XFor) || "unknown".equalsIgnoreCase(XFor)) {
+            XFor = request.getRemoteAddr();
+        }
+        return XFor;
     }
 
 }
